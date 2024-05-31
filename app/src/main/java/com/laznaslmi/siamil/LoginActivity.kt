d6181cc085ecda4d2741852feef6d9718158315d
Muhamad Adib Minanurohman <inanadib084@gmail.com>
2024-05-31 09:05:33 +0700

    add : layout login

--- app/src/main/java/com/laznaslmi/siamil/LoginActivity.kt
+++ app/src/main/java/com/laznaslmi/siamil/LoginActivity.kt
@@ -1,30 +1,20 @@
 package com.laznaslmi.siamil
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class OnBoardingActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_on_boarding)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.onboarding)) { v, insets ->
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Misalnya ada tombol untuk berpindah ke LoginActivity
-        val btnMulai: Button = findViewById(R.id.btn_mulai)
-        btnMulai.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
         }
     }
 }
